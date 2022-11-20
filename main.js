626bbae1f23b76ffe5bb96b4080877736c5566c9
gilangpralaya005 <116853655+gilangpralaya005@users.noreply.github.com>
2022-11-20 14:45:07 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var vue_app = new Vue({
+    el: '#app',
+    //Letakkan JAVASCRIPT disini
+    data() {
+        return {
+          items: [
+            { id: 1, first_name: 'Dickerson', last_name: 'Macdonald' },
+            { id: 2, first_name: 'Larsen', last_name: 'Shaw' },
+            { id: 3, first_name: 'Geneva', last_name: 'Wilson' },
+            { id: 4, first_name: 'Jami', last_name: 'Carney' }
+          ]
+        }
+      }
+ });
